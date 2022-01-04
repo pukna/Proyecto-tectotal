dbb6c9f3babdeef04c2a866a6c9fb835b0576dd3
unknown <pukna_9009@hotmail.com>
2022-01-04 10:27:25 -0500

    #3 creacion de seeder usuario y articulos

---
+++ database/seeders/ArticlesTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Article;
+use Illuminate\Database\Seeder;
+
+class ArticlesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Vaciar la tabla.
+        Article::truncate();
+        $faker = \Faker\Factory::create();
+        // Crear artículos ficticios en la tabla
+        for ($i = 0; $i < 50; $i++) {
+            Article::create([
+                'name' => $faker->sentence,
+                'amount' => $faker->randomDigit(),
+            ]);
+        }
+    }
+}
