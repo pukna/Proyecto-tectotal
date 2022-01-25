d675d30b7c3b0387cf7be1239b497b6e79d284ac
unknown <pukna_9009@hotmail.com>
2022-01-25 15:00:42 -0500

    configuracion mideware y autentificacion de jwt

--- routes/api.php
+++ routes/api.php
@@ -16,11 +16,23 @@ use Illuminate\Support\Str;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('register', 'App\Http\Controllers\UserController@register');
+Route::post('login', 'App\Http\Controllers\UserController@authenticate');
+
+Route::group(['middleware' => ['jwt.verify']], function() {
+
+    Route::get('user', 'App\Http\Controllers\UserController@getAuthenticatedUser');
+
+// Articulos
 Route::get('articles', 'App\Http\Controllers\ArticleController@index');
 Route::get('articles/{article}', 'App\Http\Controllers\ArticleController@show');
 Route::post('articles', 'App\Http\Controllers\ArticleController@store');
 Route::put('articles/{article}', 'App\Http\Controllers\ArticleController@update');
 Route::delete('articles/{article}', 'App\Http\Controllers\ArticleController@delete');
+// Proveedores
+
+
+});
 
 //Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //    return $request->user();
